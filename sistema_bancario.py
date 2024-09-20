2ef3c443027ee31f1d194322102b151c999a5e51
Ronaldo Melo <118833434+xrmelo@users.noreply.github.com>
2024-09-20 11:49:16 -0300

    Update sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -8,30 +8,59 @@ def menu():
 ================
 """
     print(menu)
-
-
-###---------------------------------------------------------------------
-
+    opcao = int(input("Selecione uma opção: "))
+    return opcao
+#------------------------------------------------------------------------------------------------------------------------
+def depositar(saldo,extrato):
+    print("\n### Depositar ###")
+    
+    while True:
+        valor = float(input("\nInforme o valor do depósito ou 0 para cancelar: "))
+        if valor == 0:
+            break
+        elif valor > 0:
+            saldo += valor
+            print(f"\nDeposito de R$ {valor:.2f} realizado com sucesso.\nSeu saldo é R$ {saldo:.2f}")
+        else:
+            print("\nInforme um valor positivo ou 0 para cancelar ")
+        
+    # end while
+    # extrato += f"Deposito: R$ {valor:.2f}\n"
+   
+    return saldo, extrato
+#------------------------------------------------------------------------------------------------------------------------
+def sacar(saldo,extrato):
+    print("em desenvolvimento")
+    extrato += f"Saque...: R$ {valor:.2f}\n"
+    return saldo,extrato
+#------------------------------------------------------------------------------------------------------------------------
+def extrato(saldo,extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+#------------------------------------------------------------------------------------------------------------------------
+#                                                  MAIN
+#------------------------------------------------------------------------------------------------------------------------
 QTD_LIMITE_SAQUE_POR_DIA = 3
 LIMITE_SAQUE         = 500
 saldo = 0
 extrato = ""
 qtd_saques = 0
-
 while True:
-    menu()
-    opcao = int(input("Selecione uma opção: "))
+    print(f"\nSeu saldo é R$ {saldo:.2f}")
+    opcao = menu()   
     if opcao == 1:
-        valor = float(input("Informe o valor do depósito: "))
-        # saldo, extrato = depositar(saldo, valor, extrato)
+       saldo, extrato = depositar(saldo,extrato)
     elif opcao == 2:
-        valor = float(input("Informe o valor do saque: "))
+        print("Sacar em desenvolvimento")
+        #valor = float(input("Informe o valor do saque: "))
         # saldo, extrato = sacar(saldo=saldo,valor=valor,extrato=extrato,limite=limite,numero_saques=numero_saques, limite_saques=LIMITE_SAQUES
     elif opcao == 3:
-        print("Extrato")
-        ##exibir_extrato(saldo, extrato=extrato)
+        print("Extrato em desenvolvimento")
+        # extrato(saldo,extrato)
     elif opcao == 0:
         break
     else:
         print("Opção inválida")
-# end while
+#------------------------------------------------------------------------------------------------------------------------
